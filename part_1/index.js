2db2260431edc893af6949b23547664b7676fdb6
Mohimkhan <mohimkhan652@gmail.com>
2025-02-11 19:48:07 +0600

    feat: add pop method to MyOwnArray class

--- part_1/index.js
+++ part_1/index.js
@@ -9,10 +9,20 @@ class MyOwnArray {
     this.length++;
     return this.length;
   }
+
+  pop() {
+    const lastEle = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return lastEle;
+  }
 }
 
 const arr1 = new MyOwnArray();
 
 arr1.push("Hello");
+arr1.push("world");
+arr1.push("bangladesh");
+arr1.pop();
 
 console.log(arr1);
