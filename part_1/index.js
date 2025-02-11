d124912093168b5221236955e9624b9e6c85cce4
Mohimkhan <mohimkhan652@gmail.com>
2025-02-11 19:45:57 +0600

    feat: implement push method for MyOwnArray class

--- part_1/index.js
+++ part_1/index.js
@@ -3,8 +3,16 @@ class MyOwnArray {
     this.length = 0;
     this.data = {};
   }
+
+  push(ele) {
+    this.data[this.length] = ele;
+    this.length++;
+    return this.length;
+  }
 }
 
 const arr1 = new MyOwnArray();
+
+arr1.push("Hello");
 
 console.log(arr1);
