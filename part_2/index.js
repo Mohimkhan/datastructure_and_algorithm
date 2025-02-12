180e3d9fdaa9f222de1f42bff4bd6a43cc7283a9
Mohimkhan <mohimkhan652@gmail.com>
2025-02-12 12:10:01 +0600

    feat: add isPalindrome function to check if a string is a palindrome

--- part_2/index.js
+++ part_2/index.js
@@ -6,5 +6,18 @@ const reverseString = (str) => {
     return str.split("").reverse().join("");
 }
 
-console.log(reverseString("mohim"));
+// console.log(reverseString("mohim"));
 
+/**
+ * ? Q:[palindrome]
+ */
+
+const isPalindrome = (str) => {
+    if (str.toLowerCase() === reverseString(str.toLowerCase())) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(isPalindrome("faf"));
