177e81a00482c64bc6c30398c65ba229ece55c8e
Mohimkhan <mohimkhan652@gmail.com>
2025-02-18 18:44:58 +0600

    feat: implement push method in LinkList class

--- part_3/index.js
+++ part_3/index.js
@@ -1,14 +1,31 @@
 class Node {
-    constructor(value) {
-        this.head = value;
-        this.next = null;
-    }
+  constructor(value) {
+    this.head = value;
+    this.next = null;
+  }
 }
 
 class LinkList {
-    constructor(value) {
-        this.head = new Node(value);
-        this.tail = this.head;
-        this.length = 1;
+  constructor(value) {
+    this.head = new Node(value);
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  push(value) {
+    let newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
     }
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+  }
 }
+
+const myLinkList = new LinkList(1);
+myLinkList.push(10);
+console.log(myLinkList);
