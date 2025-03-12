5d4994445c2c49b1ac98b1fef420ffe5c3786baa
Mohimkhan <mohimkhan652@gmail.com>
2025-03-12 19:50:28 +0600

    feat: implement enqueue method in Queue class to add elements

--- part_6/queue.js
+++ part_6/queue.js
@@ -12,8 +12,17 @@ class Queue {
     this.last = newNode;
     this.length = 1;
   }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    this.first.next = newNode;
+    this.last = newNode;
+    this.length++;
+    return this.last;
+  }
 }
 
 const queue = new Queue(1);
-
+queue.enqueue(2);
+queue.enqueue(3);
 console.log({queue});
