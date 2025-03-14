7b856da2b989208a55b283d1d5dd8000ccf2c6cc
Mohimkhan <mohimkhan652@gmail.com>
2025-03-14 19:43:29 +0600

    feat: add reverseStr function to reverse a string using a stack

--- part_7/index.js
+++ part_7/index.js
@@ -8,13 +8,13 @@ const isValidParenthesis = (str) => {
 
   for (let char of str) {
     if (brackets[char]) {
-        stack.push(char);
+      stack.push(char);
     } else {
-        const top = stack.pop();
+      const top = stack.pop();
 
-        if (!top || brackets[top] !== char) {
-            return false;
-        } 
+      if (!top || brackets[top] !== char) {
+        return false;
+      }
     }
   }
 
@@ -25,3 +25,20 @@ console.log(isValidParenthesis("(){}[]")); // true
 console.log(isValidParenthesis("([)]")); // false
 console.log(isValidParenthesis("()")); // true
 console.log(isValidParenthesis("(")); // false
+
+const reverseStr = (str) => {
+  const stack = [];
+  let output = "";
+
+  for (let char of str) {
+    stack.push(char);
+  }
+
+  for (let char of str) {
+    output += stack.pop();
+  }
+
+  return output;
+};
+
+console.log(reverseStr("hello world"));
