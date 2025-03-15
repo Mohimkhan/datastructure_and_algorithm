bd1646af79f604943fda31ddc35c0bbfe47f69ad
Mohimkhan <mohimkhan652@gmail.com>
2025-03-15 21:02:47 +0600

    feat: add hash function to HashTable class for key mapping

--- part_8/hashTable.js
+++ part_8/hashTable.js
@@ -2,4 +2,18 @@ class HashTable {
   constructor(size = 5) {
     this.keyMap = new Array(size);
   }
+
+  _hash(key) {
+    let sum = 0;
+
+    const PRIME_NUMBER = 31;
+
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      const charCode = key.charCodeAt(0) - 96;
+
+      sum = (sum * PRIME_NUMBER + charCode) % this.keyMap.length;
+    }
+
+    return sum;
+  }
 }
