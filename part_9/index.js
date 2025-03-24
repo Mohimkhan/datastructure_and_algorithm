a4c09548960a6f1b135a7c5963e5be598af87a20
Mohimkhan <mohimkhan652@gmail.com>
2025-03-24 20:37:44 +0600

    feat: add twoSum function for finding indices of two numbers that add up to a target

--- part_9/index.js
+++ part_9/index.js
@@ -1,12 +1,31 @@
 const wordCounter = (text, word) => {
-    // attempt 1
+  // attempt 1
 
-    const regex = new RegExp(`${word}`, "ig");
-    return text.match(regex).length;
+  const regex = new RegExp(`${word}`, "ig");
+  return text.match(regex).length;
 
-    // attempt 2
-    // return text.split(" ").filter((str) => str.toLowerCase() === word.toLowerCase()).length;
-}
-
+  // attempt 2
+  // return text.split(" ").filter((str) => str.toLowerCase() === word.toLowerCase()).length;
+};
 
 console.log(wordCounter("Hello my name name name is mohim", "name"));
+
+const twoSum = (nums, target) => {
+  const obj = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+
+    const compliment = target - num;
+
+    if (compliment in obj && obj[compliment] !== i) {
+      return [obj[compliment], i];
+    }
+
+    obj[num] = i;
+  }
+
+  return [];
+};
+
+console.log(twoSum([2, 7, 11, 15], 22));
