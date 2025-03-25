668650828405a6d4532d8f9dbe068a194e349377
Mohimkhan <mohimkhan652@gmail.com>
2025-03-25 19:51:02 +0600

    feat: implement insert method for BST class to add new nodes

--- part_10/index.js
+++ part_10/index.js
@@ -10,9 +10,46 @@ class BST {
   constructor() {
     this.root = null;
   }
+
+  insert(value) {
+    const newNode = new Node(value);
+
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+
+    let temp = this.root;
+
+    while (true) {
+      if (newNode.value === temp.value) {
+        return undefined;
+      }
+
+      if (newNode.value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        } else {
+          temp = temp.left;
+        }
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        }
+
+        temp = temp.right;
+      }
+    }
+  }
 }
 
-
 const tree = new BST();
-
-console.log(tree)
+tree.insert(5);
+tree.insert(8);
+tree.insert(3);
+tree.insert(1);
+tree.insert(7);
+tree.insert(9);
+console.log(tree);
