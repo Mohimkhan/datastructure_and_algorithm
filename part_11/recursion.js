845053f57865ac8e6219c3eb4b58eb0e0a05245f
Mohimkhan <mohimkhan652@gmail.com>
2025-04-01 12:02:27 +0600

    feat: add fibonacci function to demonstrate recursion

--- part_11/recursion.js
+++ part_11/recursion.js
@@ -12,3 +12,14 @@ countDown(5);
 const factorial = (num) => (num === 0 ? 1 : num * factorial(num - 1));
 
 console.log(factorial(5));
+
+// Fibonacci Sequence
+
+const fibonacci = (n) => {
+  if (n < 2) {
+    return n;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+};
+
+console.log(fibonacci(7));
